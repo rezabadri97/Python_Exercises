186e70fec2373b0386bbd30de321c25549b1dfd6
Rezabadri97i <Rezabadri97@gmail.com>
2026-02-14 07:40:39 +0330

    Practice arithmetic operators with a math riddle

---
+++ main.py
@@ -0,0 +1,11 @@
+first_number=input("Choose a number between 1-9? ")
+first_number=int(first_number)
+number=first_number
+number*=2
+number+=8
+number+=first_number
+number-=2
+number/=3
+number-=first_number
+number*=4
+print(number)
