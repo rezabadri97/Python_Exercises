10ff859918e243be0123652d0768761edabeacf9
Rezabadri97i <Rezabadri97@gmail.com>
2026-02-18 07:08:46 +0330

    Solved Exercise 17: Implemented default parameters and string conversion in labels

---
+++ exercise_17.py
@@ -0,0 +1,9 @@
+def creat_lable(element_id,prefix="BIM-"):
+    result=prefix+str(element_id)
+    return result
+E_I_01=5050
+E_I_02=7070
+final_result=creat_lable(E_I_01)
+print(f"Lable: {final_result}")
+final_result=creat_lable(E_I_02,"Revit-")
+print(f"Lable: {final_result}")
