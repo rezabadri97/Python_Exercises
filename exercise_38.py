540191a34098a7e6545f6d6bf55695851acabcb4
Rezabadri97i <Rezabadri97@gmail.com>
2026-02-19 18:48:18 +0330

    Fix method call and return statement in inheritance

---
+++ exercise_38.py
@@ -0,0 +1,15 @@
+class Wall:
+    def __init__(self, name, length, height):
+        self.name=name
+        self.length=length
+        self.height=height
+    def get_area(self):
+        wall_area=self.length*self.height
+        return wall_area
+class Curtainwall(Wall):
+    def __init__(self, name, length, height, panel_count):
+        super().__init__(name, length, height)
+        self.panel_count=panel_count
+facade=Curtainwall("Main_Facade", 20.0, 4.0, 10.0)
+facade_area=facade.get_area()
+print(f"Facade Report -> Name: {facade.name} | Area: {facade_area} sqm | Panels: {facade.panel_count}")
