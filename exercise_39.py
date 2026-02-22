3116a219f1c3b336e72e74797c45ee91e5504d30
Rezabadri97i <Rezabadri97@gmail.com>
2026-02-22 06:01:58 +0330

    Implement polymorphism for CurtainWall area calculation

---
+++ exercise_39.py
@@ -0,0 +1,18 @@
+class Wall:
+    def __init__(self,name,length,height):
+        self.name=name
+        self.length=length
+        self.height=height
+    def get_area(self):
+        wall_area=self.length*self.height
+        return wall_area
+class Curtainwall(Wall):
+    def __init__(self, name, length, height,panel_count):
+        super().__init__(name,length,height)
+        self.panel_count=panel_count
+    def get_area(self):
+        wall_area=self.length*self.height*0.95
+        return wall_area
+facade=Curtainwall("Main_Facade", 20.0, 4.0, 10.0)
+facade_Area=facade.get_area()
+print(f"Facade Area: {facade_Area}")
