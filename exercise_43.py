27e6e50cb0861279abd35a8caf7e5db8d08cf0ea
Rezabadri97i <Rezabadri97@gmail.com>
2026-02-23 20:34:01 +0330

    feat: remove duplicate family instances using set

---
+++ exercise_43.py
@@ -0,0 +1,4 @@
+family_instances=["Door_Single", "Window_Standard", "Door_Single", "Column_Round", "Window_Standard", "Wall_Generic"]
+unique_families=set(family_instances)
+print(len(unique_families))
+print(unique_families)
